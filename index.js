9187dc17797d7d184d4b970df7b37291098bbb8f
Diaa-E <blarb21@yahoo.com>
2022-12-09 23:37:38 +0200

    Create model object structure

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 "use strict";
+
+const modelFactory = () => {
+
+    const model = {
+        mesh: {
+            details : "",
+            items : [],
+            meshType: "",
+            polyTier : "",
+            gameReady: true,
+            subdivision: false,
+            edgeSplit: "",
+            realWorldScale: true,
+            polygons: null,
+            polygonType: "",
+            vertices: null,
+        },
+        rig: {
+            rigged: false,
+            rigNotes: "",
+        },
+        materials: [],
+        textures: {
+            format: "",
+            workflow: "",
+            normals: "",
+            idMap: false,
+            packedRgb: true,
+            Uv: "",
+            maps: [],
+        },
+        render: {
+            thumb: {
+                engine: "",
+                software: "",
+                version: null,
+            },
+            preview: {
+                engine: "",
+                software: "",
+                version: null,
+            }
+        }
+    }
+}
