3499d8fc8fb93451ff8af957a290850fa8c134a8
Diaa-E <blarb21@yahoo.com>
2024-09-12 21:30:25 +0300

    Add text area component

---
+++ src/components/TextArea.js
@@ -0,0 +1,61 @@
+import { createElement } from "../utils/createElement";
+import { v4 as generateId } from "uuid";
+import styles from "./TextArea.module.css";
+
+export default function TextArea(props = {
+
+    text: "",
+    placeholder: "",
+    rows: 10,
+    cols: 30,
+})
+{
+    const id = generateId();
+
+    props = {
+        text: "text area",
+        placeholder: "text area",
+        rows: 10,
+        cols: 30,
+        ...props
+    };
+
+    const label = createElement(
+        "label",
+        {
+            for: id,
+            class: styles["text-area-label"]
+        },
+        [
+            props.text
+        ]
+    );
+    
+    const textArea = createElement(
+        "textarea",
+        {
+            id: id,
+            placeholder: props.placeholder,
+            class: styles["text-area"],
+            resize: false,
+            rows: props.rows,
+            cols: props.cols,
+        },
+        [
+
+        ]
+    );
+
+    const divContainer = createElement(
+        "div",
+        {
+            class: styles["text-area-field"]
+        },
+        [
+            label,
+            textArea
+        ]
+    );
+
+    return divContainer;
+}
