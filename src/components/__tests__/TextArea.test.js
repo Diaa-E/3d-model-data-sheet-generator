4f162363cd2840291b53df6b8453fdd04e105d2d
Diaa-E <blarb21@yahoo.com>
2024-09-12 21:40:40 +0300

    Write unit tests for text area component

---
+++ src/components/__tests__/TextArea.test.js
@@ -0,0 +1,106 @@
+import TextArea from "../TextArea";
+
+describe("Text Area Component", () => {
+
+    beforeEach(() => {
+
+        cleanup();
+        jest.resetAllMocks();
+    });
+
+    it("Renders a text area with a label", () => {
+
+        render(TextArea({
+            cols: 30,
+            rows: 10,
+            onChange: () => {},
+            placeholder: "text area",
+            text: "text area"
+        }));
+
+        const textArea = document.querySelector("textarea");
+        const label = document.querySelector("label");
+
+        expect(textArea).not.toBeNull();
+        expect(label).not.toBeNull();
+    });
+
+    it("Label uses text from text props", () => {
+
+        render(TextArea({
+            cols: 30,
+            rows: 10,
+            onChange: () => {},
+            placeholder: "text area",
+            text: "text area"
+        }));
+
+        const label = document.querySelector("label");
+
+        expect(label.textContent).toBe("text area");
+    });
+
+    it("Uses cols attribute from props", () => {
+        
+        render(TextArea({
+            cols: 50,
+            rows: 10,
+            onChange: () => {},
+            placeholder: "text area",
+            text: "text area"
+        }));
+
+        const textArea = document.querySelector("textarea");
+
+        expect(textArea.cols).toBe(50);
+    });
+
+    it("Uses rows attribute from props", () => {
+
+        render(TextArea({
+            cols: 50,
+            rows: 20,
+            onChange: () => {},
+            placeholder: "text area",
+            text: "text area"
+        }));
+
+        const textArea = document.querySelector("textarea");
+
+        expect(textArea.rows).toBe(20);
+    });
+
+    it("Uses placeholder from props", () => {
+
+        render(TextArea({
+            cols: 50,
+            rows: 20,
+            onChange: () => {},
+            placeholder: "text area",
+            text: "text area"
+        }));
+
+        const textArea = document.querySelector("textarea");
+
+        expect(textArea.placeholder).toBe("text area");
+    });
+
+    it("Calls onChange function when text changes", () => {
+
+        const onChange = jest.fn();
+
+        render(TextArea({
+            cols: 50,
+            rows: 20,
+            onChange: onChange,
+            placeholder: "text area",
+            text: "text area"
+        }));
+
+        const textArea = document.querySelector("textarea");
+        changeElementValue(textArea, "some text");
+
+        expect(onChange.mock.calls).toHaveLength(1);
+        expect(textArea.value).toBe("some text");
+    });
+});
