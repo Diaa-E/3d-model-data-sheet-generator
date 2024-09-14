a92280b90068953403732fad0adc9faa0f7bb861
Diaa-E <blarb21@yahoo.com>
2024-09-14 20:39:05 +0300

    Make model info persist through reloads

--- src/components/sections/ModelInfo.js
+++ src/components/sections/ModelInfo.js
@@ -1,14 +1,18 @@
+import { getFromStorage, saveToStorage } from "../../utils/sesionStorageUtility";
 import Fieldset from "../Fieldset";
 import TextArea from "../TextArea";
 import TextInput from "../TextInput";
 
 export default function ModelInfo()
 {
-    const data = {
-
-        title: "",
-        description: ""
-    };
+    const STORAGE_KEY = "modelInfo";
+    const data = getFromStorage(
+        STORAGE_KEY,
+        {
+            title: "",
+            description: ""
+        }
+    );
 
     const titleField = TextInput({
         autocomplete: "off",
@@ -16,18 +20,22 @@ export default function ModelInfo()
         onInput: (e) => {
 
             data.title = e.target.value;
+            saveToStorage(STORAGE_KEY, data);
         },
         placeholder: "Model Title",
         required: true,
         text: "Title",
+        value: data.title
     });
 
     const descriptionField = TextArea({
         placeholder: "Model Description",
         text: "Description",
+        value: data.description,
         onInput: (e) => {
 
             data.description = e.target.value;
+            saveToStorage(STORAGE_KEY, data);
         }
     });
 
