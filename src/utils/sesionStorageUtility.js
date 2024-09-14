38752f7bcd75c4adca7b1c1be4ee770f6ccc9a10
Diaa-E <blarb21@yahoo.com>
2024-09-14 20:38:37 +0300

    Add save/retreive function for session storage

---
+++ src/utils/sesionStorageUtility.js
@@ -0,0 +1,9 @@
+export function getFromStorage(key, defaultValue)
+{
+    return JSON.parse(sessionStorage.getItem(key)) || defaultValue;
+}
+
+export function saveToStorage(key, value)
+{
+    sessionStorage.setItem(key, JSON.stringify(value));
+}
