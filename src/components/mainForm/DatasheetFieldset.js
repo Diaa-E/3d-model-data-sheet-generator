327581ea5a30341ef451a6b6114fe1ed2ad00bdc
Diaa-E <blarb21@yahoo.com>
2024-12-13 05:50:30 +0200

    Add function to generate datasheet

--- src/components/mainForm/DatasheetFieldset.js
+++ src/components/mainForm/DatasheetFieldset.js
@@ -1,18 +1,37 @@
+import Datasheet from "../Datasheet";
 import DatasheetControls from "../DatasheetControls";
 import Fieldset from "../Fieldset";
 
 export default function DatasheetFieldset()
 {
-    const datasheetControls = DatasheetControls({
+    const datasheetControls = DatasheetControls();
 
-    });
+    let datasheet = null;
 
     const fieldset = Fieldset({
         legend: "Datasheet",
         children: [
-            datasheetControls.element
+            datasheetControls.element,
         ]
     });
 
-    return { element: fieldset.element }
+    function generateDatasheet(props = {
+        title: "",
+        sets: []
+    })
+    {
+        if (datasheet !== null)
+        {
+            datasheet.element.remove();
+        }
+
+        datasheet = Datasheet({
+            title: props.title,
+            sets: props.sets
+        });
+
+        fieldset.element.append(datasheet.element);
+    }
+
+    return { element: fieldset.element, generateDatasheet: generateDatasheet }
 }
