367a9ff48a12f12e049f2598847e9c14b5ffe0c5
Diaa-E <blarb21@yahoo.com>
2024-12-15 02:36:23 +0200

    Add a copy to clipboard utility function - Write unit tests for function

---
+++ src/utils/__tests__/copyToClipboard.test.js
@@ -0,0 +1,112 @@
+import copyToClipboard from "../copyToClipboard";
+
+describe("Copy To Clipboard Utility Function", () => {
+
+    function mockClipboard()
+    {
+        let clipboard = "";
+
+        Object.assign(
+            navigator,
+            {
+                clipboard: {
+                    write: jest.fn().mockImplementation((data) => clipboard = data),
+                    read: jest.fn().mockImplementation(() => clipboard),
+                }
+            }
+        );
+
+        Object.assign(
+            global,
+            {
+                ClipboardItem: jest.fn().mockImplementation((data) => data),
+            }
+        );
+    }
+
+    function mockExecCommand()
+    {
+        Object.assign(
+            global.document,
+            {
+                execCommand: jest.fn().mockImplementation((eventType) => dispatchEvent(new Event(eventType)))
+            }
+        );
+    }
+
+    afterEach(() => {
+
+        cleanup();
+        jest.resetAllMocks();
+    });
+
+    describe("Clipboard API Case", () => {
+ 
+        beforeEach(() => {
+
+            mockClipboard();
+        });
+
+        it("Writes to clipboard using the clipboard API", () => {
+
+            const element = document.createElement("h1");
+            element.textContent = "test";
+
+            copyToClipboard(element);
+
+            expect(navigator.clipboard.write).toHaveBeenCalledTimes(1);
+        });
+
+        it("Writes data in a text/HTML Blob and a text/plain Blob as a fallback", () => {
+
+            const element = document.createElement("h1");
+            element.textContent = "test";
+
+            copyToClipboard(element);
+
+            const data = navigator.clipboard.read()[0];
+
+            expect(data["text/html"]).toBeDefined();
+            expect(data["text/html"]).toBeInstanceOf(Blob);
+
+            expect(data["text/plain"]).toBeDefined();
+            expect(data["text/plain"]).toBeInstanceOf(Blob);
+        });
+    });
+
+    // This case does not seem very testable at the moment
+    describe("execCommand case", () => {
+
+        beforeEach(() => {
+
+            Object.assign(
+                global,
+                {
+                    ClipboardItem: undefined
+                }
+            );
+            
+            mockExecCommand();
+        });
+
+        it("Triggers a copy event using execCommand", () => {
+
+            const element = document.createElement("h1");
+            element.textContent = "test";
+
+            copyToClipboard(element);
+
+            expect(document.execCommand).toHaveBeenCalledWith("copy");
+        });
+    });
+
+    describe("Error handling", () => {
+
+        it("Throws when passed in invlaid HTML element", async () => {
+    
+            // Async functions do not strictly throw like classes or regular functions, trying to catch
+            // using a throw assertion leaves an unresolved promise and the test fails to run
+            await expect(copyToClipboard(12)).rejects.toBeInstanceOf(TypeError);
+        });
+    });
+});
---
+++ src/utils/copyToClipboard.js
@@ -0,0 +1,38 @@
+import { dispatchErrorPopupEvent } from "./errorPopupEvents";
+
+export default async function copyToClipboard(element)
+{
+    if (!(element instanceof HTMLElement))
+    {
+        throw new TypeError(`Invalid element "${element}"`);
+    }
+
+    if (global.ClipboardItem && navigator.clipboard.write)
+    {
+        // HTML might not work if the pasting target does not support the html mime
+        const html = new Blob([element.innerHTML], {type: "text/html"});
+        // plain text fallback
+        const text = new Blob([element.textContent], {type: "text/plain"});
+        const data = new ClipboardItem({ [html.type]: html, [text.type]: text });
+        await navigator.clipboard.write([data]);
+    }
+    else if (document.execCommand)
+    {
+        function onCopy(e)
+        {
+            e.clipboardData.setData("text/html", element.innerHTML);
+            e.clipboardData.setData("text/plain", element.textContent);
+            e.preventDefault();
+        }
+        document.addEventListener("copy", onCopy);
+        document.execCommand("copy");
+        document.removeEventListener("copy", onCopy);
+    }
+    else
+    {
+        dispatchErrorPopupEvent({
+            dispatchingElement: element,
+            errorMsg: "This browser does not support copying to clipboard."
+        });
+    }
+}
