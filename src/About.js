bc1cd092d3e68dd41758b81004754df9268d314c
Diaa-E <blarb21@yahoo.com>
2025-05-08 21:01:29 +0300

    Update about page

--- src/About.js
+++ src/About.js
@@ -23,7 +23,7 @@ export default function About()
             class: styles["app-version"]
         },
         [
-            "Version 2.0.0"
+            "Version 2.0.1"
         ]
     );
 
@@ -63,44 +63,8 @@ export default function About()
     );
 
     const changesList = [
-        `Items added by user are now checked for invalid characters that break the target site's formatting.`,
-        `All actions that remove data now show a confirmation dialog to avoid unfortunate accidents.`,
-        `Added a nav menu that replaces the nav bar when using a small screen.`,
-        `Empty lists now show text indicating it has no items.`,
-        `Replaced edge splitting options with industry standard ones.`,
-        `Form can now be reset to default values by clicking a button.`,
-        `Added a hint text to some fieldsets.`,
-        `Required fieldsets are now flagged with an (*).`,
-        `Added dark mode.`,
-        `Improved keyboard accessibility for all controls.`,
-        `Improved buttons accessibility.`,
-        `Buttons now have readable text on wider screens.`,
-        `Overhauled the entire color palette.`,
-        `Polygon and vertex counts (generated sheet) are now comma-seperated for better readibility.`,
-        `Missing optional fields are highlighted in red on the sheet in case the user forgot to fill them.`,
-        `Generated sheet is now displayed as a part of the document with titles and lists instead of a read-only text area.`,
-        `Selected options are labeld by a check mark.`,
-        `Added an icon to the app's tab.`,
-        `Added "animation" option to the rigging fieldset.`,
-        `Added "Non-uniform polygons" option to mesh type fieldset.`,
-        `Added "Mid-poly" option to model tier fieldset.`,
-        `Aligned page content more towards the center and added a max width for wide screens.`,
-        `Softened element shadows.`,
-        `Added a model title field.`,
-        `Improved accessibility for multiple compoenents.`,
-        `Added more descriptive options to the UV fieldset.`,
-        `Model contents are now a single field an the user can freely enter all the related info like the item count.`,
-        `Texture sets are now a single field and the user can freely enter all the related info like the resolution if applicable.`,
-        `User can now scroll to invalid form fields using a link in the error notification instead of manually scrolling up.`,
-        `Successfully completed actions are now displayed in a small notification popup at the bottom of the screen.`,
-        `Errors are now displayed in a small notification popup at the bottom of the screen.`,
-        `Removed the "scaled to real world dimensions" fieldset.`,
-        `Document state is now saved to session storage to persist through reloads.`,
-        `Users can now add their own options to some fieldsets.`,
-        `Redesigned the logo.`,
-        `Added extra options to the target site options.`,
-        `Added feature to generate unformatted text (selecting none in the target site fieldset).`,
-        `Changed app structure to include a home page, a page for each form (currently only 3D model is available) and an about page.`,
+        `Fixed nav bar not highlighting active page.`,
+        `Some code improvements`,
     ];
 
     const changeListItems = changesList.map(item => {
