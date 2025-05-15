5cbe6a8a6e9f81b13c4951cee1a00d27c7f998cc
Diaa-E <blarb21@yahoo.com>
2025-05-15 23:44:57 +0300

    Write missing unit tests

--- src/components/__tests__/RadioGroup.test.js
+++ src/components/__tests__/RadioGroup.test.js
@@ -74,4 +74,50 @@ describe("Radio Group Component", () => {
 
         expect(div.textContent).not.toMatch(/no\sitems/i);
     });
+
+    it("Removes all children when removeAllButtons funciton is called", () => {
+
+        const button1 = document.createElement("button");
+        const button2 = document.createElement("button");
+        const button3 = document.createElement("button");
+
+        const radioGroupComponent = RadioGroup({
+            buttons: [button1, button2, button3],
+        });
+
+        render(radioGroupComponent.element);
+
+        expect(document.querySelectorAll("button")).toHaveLength(3);
+
+        radioGroupComponent.removeAllButtons();
+
+        expect(document.querySelectorAll("button")).toHaveLength(0);
+    });
+
+    it("Removes element with matching label text", () => {
+
+        const label1 = document.createElement("label");
+        label1.textContent = "remove1";
+
+        const label2 = document.createElement("label");
+        label2.textContent = "keep2";
+
+        const label3 = document.createElement("label");
+        label3.textContent = "keep3";
+
+        const radioGroupComponent = RadioGroup({
+            buttons: [label1, label2, label3],
+        });
+
+        render(radioGroupComponent.element);
+
+        expect(document.querySelectorAll("label")).toHaveLength(3);
+
+        radioGroupComponent.removeButton("remove1");
+
+        expect(document.querySelectorAll("label")).toHaveLength(2);
+
+        expect(document.querySelectorAll("label")[0].textContent).toBe("keep2");
+        expect(document.querySelectorAll("label")[1].textContent).toBe("keep3");
+    });
 });
